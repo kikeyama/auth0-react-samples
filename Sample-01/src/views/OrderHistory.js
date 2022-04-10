9b05a24d2dcd8134f2bba249e45d2073960a0e20
kikeyama <20974893+kikeyama@users.noreply.github.com>
2022-04-11 00:19:07 +0900

    [fix] retrieve order history

--- Sample-01/src/views/OrderHistory.js
+++ Sample-01/src/views/OrderHistory.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { Container, Row, Alert } from "reactstrap";
 
 import Highlight from "../components/Highlight";
@@ -8,10 +8,14 @@ import { getConfig } from "../config";
 
 export const OrderHistoryComponent = () => {
   const { user, getAccessTokenSilently, getAccessTokenWithPopup } = useAuth0();
+
+  const [accessToken, setAccessToken] = useState(null);
   const [userMetadata, setUserMetadata] = useState(null);
   const [err, setErr] = useState(null);
 
-  const { audience, tenantDomain } = getConfig();
+  const mounted = useRef(false)
+
+  const { audience, domain, tenantDomain } = getConfig();
 
   const handle = (e, fn) => {
     e.preventDefault();
@@ -20,25 +24,36 @@ export const OrderHistoryComponent = () => {
 
   const handleConsent = async () => {
     try {
-      await getAccessTokenWithPopup();
+      const token = await getAccessTokenWithPopup({
+        audience: `https://${tenantDomain}/api/v2/`,
+        scope: 'read:current_user',
+      });
+      setAccessToken(token);
       setErr(null);
     } catch (e) {
       console.log(e.message + ' in handleConsent');
       setErr(e.error);
     }
-
-    await getUserMetadata();
   };
 
-  const getUserMetadata = async () => {  
+  const getAccessToken = async () => {  
     try {
-      const accessToken = await getAccessTokenSilently({  // Failed with `Consent required` --> fixed by enabling `Allow Skipping User Consent`
-      //const accessToken = await getAccessTokenWithPopup({
+      const token = await getAccessTokenSilently({  // Failed with `Consent required` --> fixed by enabling `Allow Skipping User Consent`
+      //const token = await getAccessTokenWithPopup({
         audience: `https://${tenantDomain}/api/v2/`,
         scope: 'read:current_user',
       });
+      setAccessToken(token);
+      setErr(null);
+    } catch (e) {
+      console.log(e.message + ' in getAccessToken');
+      setErr(e.error);
+    }
+  };
 
-      const userDetailsByIdUrl = `https://${tenantDomain}/api/v2/users/${user.sub}`;
+  const getUserMetadata = async () => {
+    try {
+      const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
 
       const metadataResponse = await fetch(userDetailsByIdUrl, {
         headers: {
@@ -46,20 +61,29 @@ export const OrderHistoryComponent = () => {
         },
       });
 
-      const { user_metadata } = await metadataResponse.json();
-      console.log(user_metadata);
+      metadataResponse.json().then(data => {
+        setUserMetadata(data.user_metadata);
+        setErr(null);
+      });
 
-      setUserMetadata(user_metadata);
-      setErr(null);
     } catch (e) {
+      setUserMetadata(null);
       console.log(e.message + ' in getUserMetadata');
       setErr(e.error);
     }
   };
 
-  useEffect(() => {  
-    getUserMetadata();
-  }, [getAccessTokenSilently, user?.sub]);
+  useEffect(() => {
+    if (mounted.current) {
+      // componentDidUpdate
+      getUserMetadata();
+    } else {
+      // componentDidMount
+      getAccessToken();
+      mounted.current = true;
+    }
+  }, [accessToken]);
+
 
   var orders = userMetadata?.orders.map((value, index) => {
     return (
